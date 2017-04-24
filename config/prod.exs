d9fcb4857255ba03595480ed1589a2aa9f6a09ce
Sam Houston <shouston3@me.com>
2017-04-24 12:32:54 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,23 @@ use Mix.Config
 # which you typically run after static files are built.
 config :app, App.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "ldmw-app.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :app, App.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+# Configure cms database
+config :app, App.CMSRepo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("CMS_DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +73,5 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs" # removed for heroku following:
+                                  # http://www.phoenixframework.org/docs/heroku
