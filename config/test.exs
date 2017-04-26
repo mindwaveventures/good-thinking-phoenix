97b74e312a3a79cc5b8e02e9d472aee7f70f17d3
Sam Houston <shouston3@me.com>
2017-04-26 14:44:50 +0100

    trying travis tests without heroku postgres using ssl

--- config/test.exs
+++ config/test.exs
@@ -23,5 +23,5 @@ config :app, App.CMSRepo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("CMS_DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
+  ssl: false
 
