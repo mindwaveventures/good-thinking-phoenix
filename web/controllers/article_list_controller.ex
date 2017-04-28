9a7647cb5359f2da339686e86850f91cc52d42e6
Danielwhyte <danwhy@gmail.com>
2017-04-28 15:15:15 +0100

    gets all tags for a resource #43

--- web/controllers/article_list_controller.ex
+++ web/controllers/article_list_controller.ex
@@ -13,17 +13,29 @@ defmodule App.ArticleListController do
     resources =
       CMSRepo.all(article_query) ++ CMSRepo.all(link_query)
       |> Enum.sort(&(&1[:id] <= &2[:id]))
-      |> IO.inspect
 
     render conn, "index.html", resources: resources
   end
 
   def show(conn, %{"tag" => tag}) do
     article_query = create_tag_query(tag, "articles")
-    link_query = create_tag_query(tag, "resources")
+
+    link_query =
+      create_tag_query(tag, "resources")
+
+    articles =
+      article_query
+      |> CMSRepo.all
+      |> Enum.map(&(Map.merge(&1, %{type: "articles"})))
 
     resources =
-      CMSRepo.all(article_query) ++ CMSRepo.all(link_query)
+      link_query
+      |> CMSRepo.all
+      |> Enum.map(&(Map.merge(&1, %{type: "resources"})))
+
+    all_resources =
+      articles ++ resources
+      |> Enum.map(&(get_all_tags(&1)))
       |> Enum.sort(&(&1[:id] <= &2[:id]))
 
     case resources do
@@ -31,21 +43,21 @@ defmodule App.ArticleListController do
         conn
           |> put_status(404)
           |> render(App.ErrorView, "404.html")
-      _ -> render conn, "show.html", tag: tag, resources: resources
+      _ -> render conn, "show.html", tag: tag, resources: all_resources
     end
   end
 
   def create_tag_query(tag, type) do
     query = from t in "taggit_tag",
       where: t.name == ^tag,
-      join: apt in ^"#{type}_categorytag",
-      where: apt.tag_id == t.id,
+      join: cat in ^"#{type}_categorytag",
+      where: cat.tag_id == t.id,
       join: a in ^"#{type}_#{String.slice(type, 0..-2)}page",
-      where: a.page_ptr_id == apt.content_object_id
+      where: a.page_ptr_id == cat.content_object_id
 
     if type == "resources" do
       query
-        |> select([t, apt, a], %{
+        |> select([t, cat, a], %{
           id: a.page_ptr_id,
           heading: a.heading,
           url: a.resource_url,
@@ -53,10 +65,27 @@ defmodule App.ArticleListController do
         })
     else
       query
-        |> select([t, apt, a], %{
+        |> select([t, cat, a], %{
           id: a.page_ptr_id,
           heading: a.heading
         })
     end
+  end
+
+  def get_all_tags(resource) do
+    query = from t in "taggit_tag",
+      left_join: cat in ^"#{resource.type}_categorytag",
+      on: t.id == cat.tag_id,
+      left_join: cot in ^"#{resource.type}_contenttag",
+      on: t.id == cot.tag_id,
+      left_join: aut in ^"#{resource.type}_audiencetag",
+      on: t.id == aut.tag_id,
+      where: ^resource.id == cat.content_object_id
+      or ^resource.id == cot.content_object_id
+      or ^resource.id == aut.content_object_id,
+      select: t.name,
+      distinct: t.name
+
+    Map.merge(%{tags: CMSRepo.all(query)}, resource)
   end
 end
