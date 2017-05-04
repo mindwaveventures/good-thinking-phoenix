a39cc766e99a094deb680a1c4a9ef42145a727c1
Sam Houston <shouston3@me.com>
2017-05-04 14:31:59 +0100

    adding further unit tests and minor syntax changes

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -1,13 +1,8 @@
 defmodule App.ErrorView do
   use App.Web, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
-
-  def render("500.html", _assigns) do
-    "Internal server error"
-  end
+  def render("404.html", _assigns), do: "Page not found"
+  def render("500.html", _assigns), do: "Internal server error"
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
