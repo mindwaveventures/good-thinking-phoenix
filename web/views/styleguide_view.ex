d96be8b36b77757b8aa35e6b935aa2e69eb7737c
Cleop <cleo.pearson@hotmail.co.uk>
2017-05-09 17:49:46 +0100

    Creates function that renders every standard component automatically, #115

--- web/views/styleguide_view.ex
+++ web/views/styleguide_view.ex
@@ -1,3 +1,18 @@
 defmodule App.StyleGuideView do
   use App.Web, :view
+
+  def render_example_components() do
+    # Get a list of file names
+    # Filter list for those that contain the word 'example'
+    # Map over the list to remove .eex and then gets rendered in index.html.eex
+
+    elem(File.ls("./web/templates/component"), 1)
+    |> Enum.filter(fn file ->
+      String.contains?( file, "example")
+    end)
+    |> Enum.map(fn file ->
+      String.trim_trailing(file, ".eex")
+    end)
+  end
+
 end
