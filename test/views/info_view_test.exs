87d42fcf1256b2a3527de554ef15132c744ad60c
Danielwhyte <danwhy@gmail.com>
2017-05-10 11:31:43 +0100

    adds function for rendering internal links #124

--- test/views/info_view_test.exs
+++ test/views/info_view_test.exs
@@ -18,4 +18,14 @@ defmodule App.InfoViewTest do
       assert App.InfoView.renderImage(~s(<div><embed alt="hellos" embedtype="image" format="left" id="2"/></div><div><embed alt="world" embedtype="image" format="right" id="3"/></div>), conn) ==
       ~s(<div><img src="/images/LDMW_Assets_Crisis_abKUOsH.png" alt="hellos" class="fl" /></div><div><img src="/images/Screen_Shot_2017-04-20_at_17.15.42.png" alt="world" class="fr" /></div>)
     end
+
+    test "transform single link" do
+      assert App.InfoView.renderLink(~s(<a id="11" linktype="page">link</a>)) ==
+      ~s(<a href="/info/crisis">link</a>)
+    end
+
+    test "transform multiple links" do
+      assert App.InfoView.renderLink(~s(<div><a id="11" linktype="page">link</a></div><div><a id="12" linktype="page">link2</a></div>)) ==
+      ~s(<div><a href="/info/crisis">link</a></div><div><a href="/info/coming-soon">link2</a></div>)
+    end
 end
--- web/views/info_view.ex
+++ web/views/info_view.ex
@@ -7,6 +7,7 @@ defmodule App.InfoView do
 
   # Regex to match wagtail embedded image and pull out title, id and format
   @embed_regex ~r/<embed\salt=\"([^\"]+)\"\sembedtype=\"image\"\sformat=\"([^\"]+)\"\sid=+\"([^\"]+)\"\/>/
+  @link_regex ~r/<a\sid=\"([^\"]+)\"\slinktype=\"page\">([^<]+)<\/a>/
 
   def renderImage(html_string, conn) do
       Regex.replace(@embed_regex, html_string,
@@ -16,8 +17,8 @@ defmodule App.InfoView do
 
   def get_img_tag(conn, _embed, alt, align, id) do
     image_query = from i in "wagtailimages_image",
-    where: i.id == ^String.to_integer(id),
-    select: i.file
+      where: i.id == ^String.to_integer(id),
+      select: i.file
 
     # Temporary - uses image in filesystem until we have image uploads to aws
     img_src =
@@ -34,5 +35,24 @@ defmodule App.InfoView do
 
       "<img src=\"#{static_path(conn, img_src)}\" " <>
       "alt=\"#{alt}\" class=\"#{img_class}\" />"
+    end
+
+    def renderLink(html_string) do
+      Regex.replace(@link_regex, html_string, &get_a_tag/3, global: true)
+    end
+
+    def get_a_tag(_full_string, id, text) do
+      link_query = from p in "wagtailcore_page",
+        where: p.id == ^String.to_integer(id),
+        join: c in "django_content_type",
+        where: p.content_type_id == c.id,
+        select: %{
+          content: c.app_label,
+          page: p.slug
+        }
+
+        data = CMSRepo.one(link_query)
+
+        "<a href=\"/#{data.content}/#{data.page}\">#{text}</a>"
     end
 end
