6353d3be43ee3c42b0a93d5e36249dfe56efc1b0
Danielwhyte <danwhy@gmail.com>
2017-05-10 17:16:33 +0100

    style suggestions from #125

--- test/views/view_helpers_test.exs
+++ test/views/view_helpers_test.exs
@@ -1,6 +1,8 @@
 defmodule App.ViewHelpersTest do
   use App.ConnCase
 
+  import App.ViewHelpers, only: [render_image: 2, render_link: 1]
+
   setup %{conn: conn} do
     conn =
       conn
@@ -10,27 +12,37 @@ defmodule App.ViewHelpersTest do
   end
 
   test "transform single image string", %{conn: conn} do
-    assert App.ViewHelpers.renderImage(~s(<embed alt="hellos" embedtype="image" format="left" id="2"/>), conn) ==
-    ~s(<img src="/images/LDMW_Assets_Crisis_abKUOsH.png" alt="hellos" class="fl" />)
+    actual = render_image(~s(<embed alt="hellos" embedtype="image" format="left" id="2"/>), conn)
+    expected = ~s(<img src="/images/LDMW_Assets_Crisis_abKUOsH.png" alt="hellos" class="fl" />)
+
+    assert actual == expected
   end
 
   test "transform single image string - full width", %{conn: conn} do
-    assert App.ViewHelpers.renderImage(~s(<embed alt="hellos" embedtype="image" format="fullwidth" id="2"/>), conn) ==
-    ~s(<div class="w-100 tc"><img src="/images/LDMW_Assets_Crisis_abKUOsH.png" alt="hellos" class="w-50" /></div>)
+    actual = render_image(~s(<embed alt="hellos" embedtype="image" format="fullwidth" id="2"/>), conn)
+    expected = ~s(<div class="w-100 tc"><img src="/images/LDMW_Assets_Crisis_abKUOsH.png" alt="hellos" class="w-50" /></div>)
+
+    assert actual == expected
   end
 
   test "transform multiple image strings", %{conn: conn} do
-    assert App.ViewHelpers.renderImage(~s(<div><embed alt="hellos" embedtype="image" format="left" id="2"/></div><div><embed alt="world" embedtype="image" format="right" id="3"/></div>), conn) ==
-    ~s(<div><img src="/images/LDMW_Assets_Crisis_abKUOsH.png" alt="hellos" class="fl" /></div><div><img src="/images/Screen_Shot_2017-04-20_at_17.15.42.png" alt="world" class="fr" /></div>)
+    actual = render_image(~s(<div><embed alt="hellos" embedtype="image" format="left" id="2"/></div><div><embed alt="world" embedtype="image" format="right" id="3"/></div>), conn)
+    expected = ~s(<div><img src="/images/LDMW_Assets_Crisis_abKUOsH.png" alt="hellos" class="fl" /></div><div><img src="/images/Screen_Shot_2017-04-20_at_17.15.42.png" alt="world" class="fr" /></div>)
+
+    assert actual == expected
   end
 
   test "transform single link" do
-    assert App.ViewHelpers.renderLink(~s(<a id="11" linktype="page">link</a>)) ==
-    ~s(<a href="/info/crisis">link</a>)
+    actual = render_link(~s(<a id="11" linktype="page">link</a>))
+    expected = ~s(<a href="/info/crisis">link</a>)
+
+    assert actual == expected
   end
 
   test "transform multiple links" do
-    assert App.ViewHelpers.renderLink(~s(<div><a id="11" linktype="page">link</a></div><div><a id="12" linktype="page">link2</a></div>)) ==
-    ~s(<div><a href="/info/crisis">link</a></div><div><a href="/info/coming-soon">link2</a></div>)
+    actual = render_link(~s(<div><a id="11" linktype="page">link</a></div><div><a id="12" linktype="page">link2</a></div>))
+    expected = ~s(<div><a href="/info/crisis">link</a></div><div><a href="/info/coming-soon">link2</a></div>)
+
+    assert actual == expected
   end
 end
--- web/views/view_helpers.ex
+++ web/views/view_helpers.ex
@@ -13,9 +13,9 @@ defmodule App.ViewHelpers do
   @embed_regex ~r/<embed\salt=\"([^\"]+)\"\sembedtype=\"image\"\sformat=\"([^\"]+)\"\sid=+\"([^\"]+)\"\/>/
   @link_regex ~r/<a\sid=\"([^\"]+)\"\slinktype=\"page\">([^<]+)<\/a>/
 
-  def renderImage(html_string, conn) do
+  def render_image(html_string, conn) do
     Regex.replace(@embed_regex, html_string,
-    &get_img_tag(conn, &1, &2, &3, &4), global: true
+      &get_img_tag(conn, &1, &2, &3, &4), global: true
     )
   end
 
@@ -37,8 +37,7 @@ defmodule App.ViewHelpers do
         _ -> "w-50"
       end
 
-    img_tag = "<img src=\"#{static_path(conn, img_src)}\" " <>
-      "alt=\"#{alt}\" class=\"#{img_class}\" />"
+    img_tag = ~s(<img src="#{static_path(conn, img_src)}" alt="#{alt}" class="#{img_class}" />)
 
     case align do
       "fullwidth" -> "<div class=\"w-100 tc\">#{img_tag}</div>"
@@ -46,7 +45,7 @@ defmodule App.ViewHelpers do
     end
   end
 
-  def renderLink(html_string) do
+  def render_link(html_string) do
     Regex.replace(@link_regex, html_string, &get_a_tag/3, global: true)
   end
 
@@ -59,6 +58,6 @@ defmodule App.ViewHelpers do
 
     data = CMSRepo.one(link_query)
 
-    "<a href=\"/#{data.content}/#{data.page}\">#{text}</a>"
+    ~s(<a href="/#{data.content}/#{data.page}">#{text}</a>)
   end
 end
