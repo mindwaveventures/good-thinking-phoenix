11933932a3ddd5a8ec1a5ef5ddd17121156e3ef8
Cleop <cleo.pearson@hotmail.co.uk>
2017-05-11 10:12:59 +0100

    Creates function to display code snippets. #115

--- web/views/styleguide_view.ex
+++ web/views/styleguide_view.ex
@@ -1,7 +1,7 @@
 defmodule App.StyleGuideView do
   use App.Web, :view
 
-  def render_example_components() do
+  defp render_example_components() do
     # Get a list of file names
     # Filter list for those that contain the word 'example'
     # Map over the list to remove .eex and then gets rendered in index.html.eex
@@ -12,6 +12,16 @@ defmodule App.StyleGuideView do
     end)
     |> Enum.map(fn file ->
       String.trim_trailing(file, ".eex")
+    end)
+  end
+
+  defp components_to_code() do
+    elem(File.ls("./web/templates/component"), 1)
+    |> Enum.filter(fn file ->
+      String.contains?( file, "example")
+    end)
+    |> Enum.map(fn file ->
+      File.read!("./web/templates/component/#{file}")
     end)
   end
 
