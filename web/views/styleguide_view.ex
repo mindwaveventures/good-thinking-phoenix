fb01b76a08aff9ebf84270df855f8923b64ac9c3
Cleop <cleo.pearson@hotmail.co.uk>
2017-05-11 14:28:43 +0100

    Write tests for the rendering of all standard components, #115

--- web/views/styleguide_view.ex
+++ web/views/styleguide_view.ex
@@ -3,8 +3,7 @@ defmodule App.StyleGuideView do
   use App.Web, :view
 
   @doc """
-  ## Get Example Files
-
+  ## Get a list of file names and filter it for those containing the word 'example'
     iex> App.StyleGuideView.get_example_files("./test/views/example_components")
     ["primary_button_test_example.html.eex",
     "secondary_button_test_example.html.eex"]
@@ -12,37 +11,59 @@ defmodule App.StyleGuideView do
     ["example_components"]
     iex> App.StyleGuideView.get_example_files("./test/controllers")
     []
-
   """
 
   def get_example_files(file_path) do
-    # Get a list of file names and filter it for those containing the word 'example'
     elem(File.ls(file_path), 1)
     |> Enum.filter(fn file ->
       String.contains?( file, "example")
     end)
   end
 
+  @doc """
+  ## Map over the list of example files to remove .eex so it can be rendered inside another .eex file
+    iex> App.StyleGuideView.render_example_components("./test/views/example_components")
+    ["primary_button_test_example.html",
+    "secondary_button_test_example.html"]
+    iex> App.StyleGuideView.get_example_files("./test/controllers")
+    []
+  """
+
   def render_example_components(file_path) do
-    # Map over the list to remove .eex so it can be rendered inside another .eex file
     get_example_files(file_path)
     |> Enum.map(fn file ->
       String.trim_trailing(file, ".eex")
     end)
   end
 
+  @doc """
+  ## Display the Code of each Component
+    iex> App.StyleGuideView.components_to_code("./test/views/example_components")
+    [~s(<%= component "primary_button", value: "I'm a Primary Button" %>\\n),
+    ~s(<%= component "secondary_button", value: "I'm a Secondary Button" %>\\n)]
+    iex> App.StyleGuideView.components_to_code("./test/controllers")
+    []
+  """
+
   def components_to_code(file_path) do
-    # Display the code of each component
     get_example_files(file_path)
     |> Enum.map(fn file ->
       File.read!("#{file_path}/#{file}")
     end)
   end
 
+  @doc """
+  ## Render both the component and its code
+    iex> App.StyleGuideView.render_whole_component("./test/views/example_components")
+    [{"primary_button_test_example.html", ~s(<%= component "primary_button", value: "I'm a Primary Button" %>\\n)},
+    {"secondary_button_test_example.html", ~s(<%= component "secondary_button", value: "I'm a Secondary Button" %>\\n)}]
+    iex> App.StyleGuideView.components_to_code("./test/controllers")
+    []
+  """
+
   def render_whole_component(file_path) do
     component_eg = render_example_components(file_path)
     component_code = components_to_code(file_path)
-    tuple_of_lists = [component_eg, component_code]
     List.zip([component_eg, component_code])
   end
 
