d934d47a5d9e6ae8aa4ec9a9c70a98fb6acea5c8
Cleop <cleo.pearson@hotmail.co.uk>
2017-05-11 11:28:11 +0100

    Changes functions to accept custom file paths, #115

--- web/views/styleguide_view.ex
+++ web/views/styleguide_view.ex
@@ -1,32 +1,33 @@
 defmodule App.StyleGuideView do
   use App.Web, :view
 
-  defp get_example_components do
+  defp get_example_components(file_path) do
     # Get a list of file names
     # Filter list for those that contain the word 'example'
     # Map over the list to remove .eex and then gets rendered in index.html.eex
 
-    elem(File.ls("./web/templates/component"), 1)
+    elem(File.ls(file_path), 1)
     |> Enum.filter(fn file ->
       String.contains?( file, "example")
     end)
   end
 
-  defp render_example_components() do
+  defp render_example_components(file_path) do
+    get_example_components(file_path)
     |> Enum.map(fn file ->
       String.trim_trailing(file, ".eex")
     end)
   end
 
-  defp components_to_code() do
-    get_example_components()
+  defp components_to_code(file_path) do
+    get_example_components(file_path)
     |> Enum.map(fn file ->
-      File.read!("./web/templates/component/#{file}")
+      File.read!("#{file_path}/#{file}")
     end)
   end
-  def render_whole_component() do
-    component_eg = render_example_components()
-    component_code = components_to_code()
+  def render_whole_component(file_path) do
+    component_eg = render_example_components(file_path)
+    component_code = components_to_code(file_path)
     tuple_of_lists = [component_eg, component_code]
     List.zip([component_eg, component_code])
   end
