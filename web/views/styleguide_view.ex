bb53ab3d45f17ac8870043beca13ccca7ee4dc12
Cleop <cleo.pearson@hotmail.co.uk>
2017-05-11 12:16:01 +0100

    Refactor

--- web/views/styleguide_view.ex
+++ web/views/styleguide_view.ex
@@ -1,30 +1,31 @@
 defmodule App.StyleGuideView do
   use App.Web, :view
 
-  defp get_example_components(file_path) do
-    # Get a list of file names
-    # Filter list for those that contain the word 'example'
-    # Map over the list to remove .eex and then gets rendered in index.html.eex
 
+  def get_example_files(file_path) do
+    # Get a list of file names and filter it for those containing the word 'example'
     elem(File.ls(file_path), 1)
     |> Enum.filter(fn file ->
       String.contains?( file, "example")
     end)
   end
 
-  defp render_example_components(file_path) do
-    get_example_components(file_path)
+  def render_example_components(file_path) do
+    # Map over the list to remove .eex so it can be rendered inside another .eex file
+    get_example_files(file_path)
     |> Enum.map(fn file ->
       String.trim_trailing(file, ".eex")
     end)
   end
 
-  defp components_to_code(file_path) do
-    get_example_components(file_path)
+  def components_to_code(file_path) do
+    # Display the code of each component
+    get_example_files(file_path)
     |> Enum.map(fn file ->
       File.read!("#{file_path}/#{file}")
     end)
   end
+
   def render_whole_component(file_path) do
     component_eg = render_example_components(file_path)
     component_code = components_to_code(file_path)
