2ad7d7693c2a5d654c6bc2b983a8b3672f5d70b9
Danielwhyte <danwhy@gmail.com>
2017-05-17 16:48:21 +0100

    restructures deploy script and adds comments #185

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
+# Backup App and CMS data, then deploy to heroku. First argument is branch/commit from app to deploy
+# Expects cms and app to directories to share a parent directory
 
-function check_exit() {
-  if [[ $1 -ne 0 ]] ; then
-    echo "EXITING - Please fix merge conflicts"
-    exit 1
-  fi
+starting_branch="$(git branch | grep \* | cut -d ' ' -f2)"
+
+if [ -z "$1" ]; then
+  echo "No argument supplied, deploying master branch"
+  branch_to_deploy=master
+else
+  branch_to_deploy=$1
+fi
+
+# Backup cms data, then deploy to heroku
+function deploy_cms() {
+  heroku run "python manage.py dumpdata --natural-foreign --natural-primary" -a ldmw-cms > ../cms_backup.json
+  git checkout master
+  git push heroku master
+  heroku run "python manage.py migrate" -a ldmw-cms
+}
+
+# Backup app data, then deploy from commit/branch passed as first argument to heroku
+function deploy_app() {
+  heroku run "pg_dump $(heroku config:get DATABASE_URL --app ldmw-app)" -a ldmw-app > ../app_backup.sql
+  git checkout $branch_to_deploy
+  git push heroku $branch_to_deploy:master
 }
 
 cd ../cms
-heroku run "python manage.py dumpdata --natural-foreign --natural-primary" -a ldmw-cms > ../cms_backup.json
-git checkout master
-git push staging master
-heroku run "python manage.py migrate" -a ldmw-cms
+deploy_cms
 cd ../app
-heroku run "pg_dump $(heroku config:get DATABASE_URL --app ldmw-app)" -a ldmw-app > ../app_backup.sql
-git checkout $1
-git push staging $1:master
+deploy_app
+git checkout $starting_branch #reset branch back to original
