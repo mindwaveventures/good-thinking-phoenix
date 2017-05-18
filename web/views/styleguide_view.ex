a12463479b64724c20ddcdfc4b4ea77bdc26ef28
Cleop <cleo.pearson@hotmail.co.uk>
2017-05-18 16:46:45 +0100

    Creates up to date tests for #193

--- web/views/styleguide_view.ex
+++ web/views/styleguide_view.ex
@@ -3,10 +3,19 @@ defmodule App.StyleGuideView do
   use App.Web, :view
 
   @doc """
-  ## Render both the component and its code
+  ## Render all standard components with categories and code
   iex> render_whole_component("./test/support/example_components")
-  [{"buttons", "buttons/primary_button_test_example.html", ~s(<%= component "buttons/primary_button", value: "I'm a Primary Button" %>\\n)},
-  {"links", "links/secondary_button_test_example.html", ~s(<%= component "links/secondary_button", value: "I'm a Secondary Button" %>\\n)}]
+  [{"buttons",
+    [{"duplicate",
+      {"buttons/duplicate_example.html",
+       ~s(<%= component "buttons/primary_button", value: "I'm a duplicate" %>\n)}},
+     {"primary button",
+      {"buttons/primary_button_example.html",
+       ~s(<%= component "buttons/primary_button", value: "I'm a Primary Button" %>\n)}}]},
+   {"links",
+    [{"secondary button",
+      {"links/secondary_button_example.html",
+       ~s(<%= component "links/secondary_button", value: "I'm a Secondary Button" %>\n)}}]}]
   iex> render_whole_component("./test/controllers")
   []
   """
@@ -17,6 +26,18 @@ defmodule App.StyleGuideView do
     |> Enum.map(fn category -> get_category_components(file_path, category) end)
   end
 
+  @doc """
+  ## Get the components of a specified category
+  iex> get_category_components("./test/support/example_components", "buttons")
+  {"buttons",
+   [{"duplicate",
+     {"buttons/duplicate_example.html",
+      ~s(<%= component "buttons/primary_button", value: "I'm a duplicate" %>\n)}},
+    {"primary button",
+     {"buttons/primary_button_example.html",
+      ~s(<%= component "buttons/primary_button", value: "I'm a Primary Button" %>\n)}}]}
+  """
+
   def get_category_components(file_path, category) do
       category_components =
         "#{file_path}/#{category}"
@@ -25,6 +46,15 @@ defmodule App.StyleGuideView do
 
       {category, category_components}
   end
+
+  @doc """
+  ## Get the details of a given component
+  ## (file name to render and code inside the file)
+  iex> get_component_details("./test/support/example_components", "buttons", "primary_button")
+  {"primary button",
+   {"buttons/primary_button_example.html",
+    ~s(<%= component "buttons/primary_button", value: "I'm a Primary Button" %>\n)}}
+  """
 
   def get_component_details(file_path, category, component) do
     formatted_component = String.replace(component, "_", " ")
@@ -36,7 +66,8 @@ defmodule App.StyleGuideView do
   end
 
   @doc """
-  ## Trim file path to get only the category folder
+  ## Search all example files and compile a list of all of their categories
+  ## Removing any duplicated category names
   iex> get_category_list("./test/support/example_components")
   ["buttons", "links"]
   iex> get_category_list("./test/controllers")
@@ -50,6 +81,15 @@ defmodule App.StyleGuideView do
     |> Enum.map(&(Enum.at(&1, -2)))
     |> Enum.uniq
   end
+
+  @doc """
+  ## Search for all files with example in in the category file path
+  ## Then remove the tail so you're left with the component name only
+  iex> get_component_names("./test/support/example_components/buttons")
+  ["duplicate", "primary_button"]
+  iex> get_component_names("./test/controllers")
+  []
+  """
 
   def get_component_names(category_file_path) do
     category_file_path
