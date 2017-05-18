c915fd4f4f9837e2183df40040e1acf1a64193b9
Sam Houston <shouston3@me.com>
2017-05-18 17:18:34 +0100

    configuring the controllers to send in right format for the new google script

--- web/controllers/spreadsheet_controller.ex
+++ web/controllers/spreadsheet_controller.ex
@@ -2,24 +2,24 @@ defmodule App.SpreadsheetController do
   use App.Web, :controller
 
   @http Application.get_env :app, :http
-  @google_sheet_url_email Application.get_env :app, :google_sheet_url_email
-  @google_sheet_url_suggestion Application.get_env(
-    :app, :google_sheet_url_suggestion)
+  @google_sheet_url Application.get_env :app, :google_sheet_url
 
   def submit_email(conn, %{"suggestions" => %{"suggestions" => suggestions}}) do
-    handle_email conn, :suggestions, suggestions, @google_sheet_url_suggestion
+    handle_email conn, :suggestions, suggestions, @google_sheet_url
   end
 
   def submit_email(conn, %{"email" => %{"email" => email}}) do
-    handle_email conn, :email, email, @google_sheet_url_email
+    handle_email conn, :emails, email, @google_sheet_url
   end
 
   defp handle_email(conn, type, data, url) do
     @http.post_spreadsheet data, url, type
 
     message = case type do
-      :email -> "Email address entered successfully!"
-      :suggestions -> "Thank you for your input, it will be used to improve and develop the service further. Let us know if you have any more feedback"
+      :emails -> "Email address entered successfully!"
+      :suggestions -> "Thank you for your input, it will "
+                   <> "be used to improve and develop the service further. "
+                   <> "Let us know if you have any more feedback"
     end
 
     conn
