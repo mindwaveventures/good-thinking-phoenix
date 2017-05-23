c52c34581d37a07dbe3452c876b521cf0adf21df
Sam Houston <shouston3@me.com>
2017-05-23 10:54:37 +0100

    adding tests and improving coverage

--- test/controllers/spreadsheet_controller_test.exs
+++ test/controllers/spreadsheet_controller_test.exs
@@ -1,15 +1,15 @@
 defmodule App.SpreadsheetControllerTest do
   use App.ConnCase, async: false
 
-  test "submit_email of email address", %{conn: conn} do
-    params = %{"email" => %{"email" => "test@me.com"}}
-    conn = post conn, spreadsheet_path(conn, :submit_email, params)
-    assert html_response(conn, 302)
-  end
-
-  test "submit_email of suggestions", %{conn: conn} do
-    params = %{"suggestions" => %{"suggestions" => "suggestions"}}
-    conn = post conn, spreadsheet_path(conn, :submit_email, params)
-    assert html_response(conn, 302)
+  test "submit of feedback", %{conn: conn} do
+    [%{"email" => %{"email" => "test@me.com"}},
+     %{"suggestions" => %{"suggestions" => "suggestions"}},
+     %{"feedback" => %{"question" => "question",
+                       "feedback1" => "feedback1",
+                       "feedback2" => "feedback2"}}]
+    |> Enum.each(fn params ->
+      conn = post conn, spreadsheet_path(conn, :submit, params)
+      assert html_response(conn, 302)
+    end)
   end
 end
