4014a017c914dcefb7d720c4503195fca719bc59
Danielwhyte <danwhy@gmail.com>
2017-05-24 17:14:18 +0100

    filters are hidden in select blocks with js #226

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -28,3 +28,27 @@ document.querySelector(".sidebar-overlay").addEventListener('click', function() 
 document.querySelector("#filter_filter").addEventListener('click', function() {
   document.querySelector("body").classList.toggle("stop-scrolling");
 });
+
+document.querySelector(".sidebar-buttons").classList.add("absolute", "bottom-2");
+
+Array.prototype.forEach.call(document.querySelectorAll(".filter-arrow"), function(el) {
+  el.classList.remove("filter-arrow-hide");
+});
+
+["category", "audience", "content"].forEach(function(el) {
+  document.querySelector("." + el + "-filters").classList.add("hide-filters", "br1", "w-80", "absolute", "bg-white", "z-1", "overflow-scroll");
+  document.querySelector(".select-" + el + "-filters").classList.add("select-filters");
+  document.querySelector(".select-" + el + "-filters").addEventListener("click", function() {
+    document.querySelector("." + el + "-filters").classList.toggle("hide-filters");
+    document.querySelector("." + el + "-filters").classList.toggle("shadow-2");
+    document.querySelector("." + el + "-filters").classList.toggle("h5");
+    document.querySelector(".sidebar").addEventListener("click", function hideAgain(e){
+      if (e.path.indexOf(document.querySelector(".select-" + el + "-filters")) == -1 && e.path.indexOf(document.querySelector("." + el + "-filters")) == -1){
+        document.querySelector("." + el + "-filters").classList.add("hide-filters");
+        document.querySelector("." + el + "-filters").classList.remove("shadow-2");
+        document.querySelector("." + el + "-filters").classList.remove("h5");
+        document.querySelector(".sidebar").removeEventListener("click", hideAgain);
+      }
+    });
+  });
+});
