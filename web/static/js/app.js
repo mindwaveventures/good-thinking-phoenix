880be8eec3904c87251b02bafc07ac94e20048e1
Danielwhyte <danwhy@gmail.com>
2017-05-25 10:24:04 +0100

    adds javascript for sidebar filters boxes #226

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,36 +19,4 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
-
-document.querySelector(".sidebar-overlay").addEventListener('click', function() {
-  document.querySelector("#filter_filter").checked = false;
-  document.querySelector("body").classList.remove("stop-scrolling");
-});
-
-document.querySelector("#filter_filter").addEventListener('click', function() {
-  document.querySelector("body").classList.toggle("stop-scrolling");
-});
-
-document.querySelector(".sidebar-buttons").classList.add("absolute", "bottom-2");
-
-Array.prototype.forEach.call(document.querySelectorAll(".filter-arrow"), function(el) {
-  el.classList.remove("filter-arrow-hide");
-});
-
-["category", "audience", "content"].forEach(function(el) {
-  document.querySelector("." + el + "-filters").classList.add("hide-filters", "br1", "w-80", "absolute", "bg-white", "z-1", "overflow-scroll");
-  document.querySelector(".select-" + el + "-filters").classList.add("select-filters");
-  document.querySelector(".select-" + el + "-filters").addEventListener("click", function() {
-    document.querySelector("." + el + "-filters").classList.toggle("hide-filters");
-    document.querySelector("." + el + "-filters").classList.toggle("shadow-2");
-    document.querySelector("." + el + "-filters").classList.toggle("h5");
-    document.querySelector(".sidebar").addEventListener("click", function hideAgain(e){
-      if (e.path.indexOf(document.querySelector(".select-" + el + "-filters")) == -1 && e.path.indexOf(document.querySelector("." + el + "-filters")) == -1){
-        document.querySelector("." + el + "-filters").classList.add("hide-filters");
-        document.querySelector("." + el + "-filters").classList.remove("shadow-2");
-        document.querySelector("." + el + "-filters").classList.remove("h5");
-        document.querySelector(".sidebar").removeEventListener("click", hideAgain);
-      }
-    });
-  });
-});
+import "./sidebar"
