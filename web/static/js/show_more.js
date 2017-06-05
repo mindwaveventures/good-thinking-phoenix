c9ca36dc9f2d778949cb34196a6394aa0b6f080f
Sam Houston <shouston3@me.com>
2017-06-05 14:44:05 +0100

    adding in the button in phoenix templating with a default class of dn-important and removing this when js is running

--- web/static/js/show_more.js
+++ web/static/js/show_more.js
@@ -3,20 +3,14 @@ selectAll('#results > div > div').forEach(function (el, i) {
     toggleClasses(el, ['dn']);
   }
 });
-var see_more_div = document.createElement('div');
-var see_more_button = document.createElement('button');
-see_more_button.innerHTML = 'See More';
-see_more_div.id = 'see_more';
-see_more_button.classList = 'pointer'
-see_more_div.appendChild(see_more_button);
-select('#results').appendChild(see_more_div);
 function handle_see_more(e) {
   selectAll('#results > div > div').forEach(function (el, i) {
     if(el.id.substring(0, 9) === 'resource_') {
       toggleClasses(el, ['dn']);
     }
-  })
-  toggleClasses(select('#see_more'), ['dn']);
+  });
+  toggleClasses(select('#see_more'), ['dn-important']);
 }
+toggleClasses(select('#see_more'), ['dn-important']);
 see_more.addEventListener('click', handle_see_more);
 
