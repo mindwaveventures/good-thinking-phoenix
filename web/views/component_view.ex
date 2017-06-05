4e437e6b4e96d6e25b5a249ece7ad43746e06fc5
Danielwhyte <danwhy@gmail.com>
2017-06-05 17:31:04 +0100

    adds doctests #265

--- web/views/component_view.ex
+++ web/views/component_view.ex
@@ -1,9 +1,20 @@
 defmodule App.ComponentView do
   use App.Web, :view
 
-  @yt_url ~r/https:\/\/www\.youtube\.com\/watch\?.*v=([^&#]+).*$/
+  @yt_url ~r/youtube\.com\/watch\?.*v=([^&#]+).*$/
   @short_yt_url ~r/youtu\.be\/([^?#]*)/
 
+  @doc """
+  ## Get Video ID from youtube url
+  iex> getVideoId("youtu.be/frg65dhyu")
+  "frg65dhyu"
+  iex> getVideoId("https://youtube.com/watch?v=hs8fm36fyrh")
+  "hs8fm36fyrh"
+  iex> getVideoId("https://youtube.com/watch?v=n475fgctdy#t=24")
+  "n475fgctdy"
+  iex> getVideoId("https://youtube.com/watch?hello=world&v=8fjry63nd#t=24")
+  "8fjry63nd"
+  """
   def getVideoId(url) do
     [@yt_url, @short_yt_url]
     |> Enum.map(&(getVideoId &1, url))
