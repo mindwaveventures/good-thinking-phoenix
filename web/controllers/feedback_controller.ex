9ee93c4f8142b97c786eb3c6d75caf0744c20150
Sam Houston <shouston3@me.com>
2017-06-06 20:34:08 +0100

    refactoring the feedback_controller

--- web/controllers/feedback_controller.ex
+++ web/controllers/feedback_controller.ex
@@ -4,7 +4,6 @@ defmodule App.FeedbackController do
   alias App.Resources, as: R
   alias App.SpreadsheetController, as: S
 
-<<<<<<< HEAD
   defp handle_bold(elem) when not is_list(elem), do: R.handle_bold elem
   defp handle_bold(elem) when is_list(elem), do: Enum.map elem, &handle_bold/1
   def index(conn, _params) do
@@ -15,23 +14,6 @@ defmodule App.FeedbackController do
       |> select([page], map(page, ^form_content))
       |> order_by([page], page.sort_order)
       |> CMSRepo.all
-=======
-  defp replace_key(map, key_old, key_new) when is_map(map) do
-    Map.new(map, fn {k, v} ->
-      if k == key_old do {key_new, v} else {k, v} end
-    end)
-  end
-
-  def index(conn, _params) do
-    form_content = [:help_text, :choices, :default_value,
-                    :required, :field_type, :label]
-    forms_query = from page in "feedback_formfield",
-      select: map(page, ^form_content),
-      order_by: page.sort_order
-    forms = forms_query
-      |> CMSRepo.all
-      |> Enum.map(&(replace_key(&1, :help_text, :question)))
->>>>>>> making use of the new get_content function
 
     feedback_content =
       [:help_text, :default_text, :intro]
@@ -42,7 +24,6 @@ defmodule App.FeedbackController do
       |> List.flatten
       |> Enum.into([:form_title])
 
-<<<<<<< HEAD
     feedback =
       "feedback_feedbackpage"
       |> select([page], ^feedback_content)
@@ -58,22 +39,6 @@ defmodule App.FeedbackController do
     render conn, "index.html", assigns
   end
 
-=======
-    render conn, "index.html",
-                 forms: Enum.map(forms, &R.handle_bold/1),
-                 form_title: R.handle_bold(feedback_content.form_title),
-                 feedback1: R.handle_bold(feedback_content.feedback1),
-                 feedback2: R.handle_bold(feedback_content.feedback2),
-                 content: %{footer: R.get_content(:footer),
-                            alpha: R.get_content(:alpha,
-                                                 {"/home/feedback/",
-                                                  "feedback_feedbackpage"}),
-                            alphatext: R.get_content(:alphatext,
-                                                 {"/home/feedback/",
-                                                  "feedback_feedbackpage"})}
-  end
-
->>>>>>> get_content is now flexible and can take atoms and lists of atoms, also refactored the use of handle_bold in the feedback controller
   def post(conn, params),
     do: S.submit conn, params, feedback_path(conn, :index) <> "#alphasection"
 end
