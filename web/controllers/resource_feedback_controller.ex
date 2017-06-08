7116a13327554f3fb2e439be3157c5af97958508
Sam Houston <shouston3@me.com>
2017-06-08 17:55:20 +0100

    refactor where we do the feedback redirect logic

--- web/controllers/resource_feedback_controller.ex
+++ web/controllers/resource_feedback_controller.ex
@@ -1,11 +1,19 @@
 defmodule App.ResourceFeedbackController do
   use App.Web, :controller
-  import App.SpreadsheetController, only: [submit: 2]
+  import App.SpreadsheetController, only: [submit: 2,
+                                           redirect_after_feedback: 2]
 
-  def resource_feedback(conn, params) do
-    %{"resource_feedback" => feedback} = params
-
+  def resource_feedback(conn, %{"resource_feedback" => feedback} = params) do
     conn
     |> submit(%{"resource_feedback" => Map.put(feedback, "id", params["id"])})
+    |> redirect_after_feedback(params["id"])
+    |> handle_json_redirect(params)
+  end
+
+  def handle_json_redirect(conn, params) do
+    case get_req_header(conn, "accept") do
+      ["application/json"] -> conn
+      _ -> redirect conn, to: homepage_path(conn, :show)
+    end
   end
 end
