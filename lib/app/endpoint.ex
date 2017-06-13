6dce596124f42714c6d2457e5f314b5e81703d97
Danielwhyte <danwhy@gmail.com>
2017-06-13 15:16:14 +0100

    tweaks to improve page load speed #359

--- lib/app/endpoint.ex
+++ lib/app/endpoint.ex
@@ -8,7 +8,7 @@ defmodule App.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :app, gzip: false,
+    at: "/", from: :app, gzip: true,
     only: ~w(css fonts images js favicon.png robots.txt)
 
   # Code reloading can be explicitly enabled under the
