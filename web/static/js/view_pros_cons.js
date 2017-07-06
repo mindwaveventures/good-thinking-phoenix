fd610f857ede9a9629854807e82386432cd4873f
katbow <katherine.bowler@gmail.com>
2017-07-06 12:37:10 +0100

    Toggle text on click #452

--- web/static/js/view_pros_cons.js
+++ web/static/js/view_pros_cons.js
@@ -1,9 +1,18 @@
-function handle_view_pros_cons(e, el) {
+function handleViewProsCons (e, el) {
   toggleClasses(select('#pros_cons' + getResourceId(el)), ['dn']);
-};
+  changeInnerHtml(el);
+}
+
+function changeInnerHtml (el) {
+  if (el.innerHTML === "View pros and cons") {
+    el.innerHTML = "Hide pros and cons";
+  } else {
+    el.innerHTML = "View pros and cons";
+  }
+}
 
 selectAll('.view_pcs').forEach(function (el) {
   el.addEventListener('click', function (e) {
-    handle_view_pros_cons(e, el);
+    handleViewProsCons(e, el);
   });
 });
